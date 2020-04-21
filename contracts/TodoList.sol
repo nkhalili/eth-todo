b92af22b05f5f034811ffd80db6cd9bb8af1b5ce
Navid Khalili <nkhalili@live.com>
2020-04-20 23:27:53 -0400

    Added contract behaviors.

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,27 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+
+  constructor() public {
+    createTask("Sample task");
+  }
+
+  // number of tasks
   uint public taskCount = 0;
+
+  // custom type Task
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  // key-value pairs in solidity
+  mapping(uint => Task) public tasks;
+
+  // _content will be persisted in memory
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
