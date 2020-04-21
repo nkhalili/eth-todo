e6fae1cf21e2a2945f2c506589248bf3fe6eaa3e
Navid Khalili <nkhalili@live.com>
2020-04-21 14:01:20 -0400

    Added toggle checkboxes + tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -22,6 +22,11 @@ contract TodoList {
     bool completed
   );
 
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
   // key-value pairs in solidity
   mapping(uint => Task) public tasks;
 
@@ -30,5 +35,13 @@ contract TodoList {
     taskCount++;
     tasks[taskCount] = Task(taskCount, _content, false);
     emit TaskCreated(taskCount, _content, false);
+  }
+
+  // to toggle task's checkbox and fire the event
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
   }
 }
